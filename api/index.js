943c4ae20d5d6b54f0fbbc37758b9cc7328a8fa1
Antonio Turis <antonituris81@gmail.com>
2021-06-19 18:04:53 +0200

    configuracion de las rutas terminadas

--- api/index.js
+++ api/index.js
@@ -1,8 +1,16 @@
 const express = require('express')
+const morgan = require('morgan')
+
+const router = require('./src/routes/index.js')
 const app = express()
-app.get('/', (req, res) => {
-	res.send('conexion ok')
+app.use(morgan('dev'))
+app.use('/api', router)
+
+app.get('/', (_req, res) => {
+	res.send('La ruta principal es: /api')
 })
-app.listen(3001, () => {
+
+
+app.listen(4001, () => {
 	console.log('servidor listo');
 });
